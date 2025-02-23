768a02a77f4eaa11909de9db980c92f24f599ee8
devon <devonjhills@gmail.com>
2025-02-22 22:07:12 -0500

    fix type error

--- app/(cms)/outstatic/[[...ost]]/page.tsx
+++ app/(cms)/outstatic/[[...ost]]/page.tsx
@@ -1,14 +1,9 @@
-import 'outstatic/outstatic.css';
-import { Outstatic } from 'outstatic';
-import { OstClient } from 'outstatic/client';
-
-interface PageProps {
-  params: {
-    ost: string[];
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  const ostData = await Outstatic();
-  return <OstClient ostData={ostData} params={params} />;
+import 'outstatic/outstatic.css'
+import { Outstatic } from 'outstatic'
+import { OstClient } from 'outstatic/client'
+ 
+export default async function Page(props: { params: Promise<{ ost: string[] }> }) {
+  const params = await props.params;
+  const ostData = await Outstatic()
+  return <OstClient ostData={ostData} params={params} />
 }
